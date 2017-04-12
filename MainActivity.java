83016b0cf1c9d30d42131439608d88d06bf32f28
Twan <tb_munster@hotmail.com>
2017-04-12 17:06:23 +0200

    werkt

--- MainActivity.java
+++ MainActivity.java
@@ -12,17 +12,13 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import com.google.zxing.Result;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
-    private ZXingScannerView mScannerView;
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void buttonClickFunction(View v)
@@ -31,31 +27,11 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         startActivity(intent);
     }
 
-    public void buttonScanClickFunction(View v){
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
+    public void buttonScanClickFunction(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), ScannerActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mScannerView.stopCamera();
-    }
-
-    @Override
-    public void handleResult(Result result) {
-        //Do anything with result here :D
-        Log.w("handleResult",result.getText( ));
-        AlertDialog.Builder builder= new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setMessage(result.getText());
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-
-        //Resume scanning uncomment below
-        //mScannerView.resumeCameraPreview(this);
-    }
 
 }
---
+++ ScannerActivity.java
@@ -0,0 +1,54 @@
+package com.casasolutions.mapsapptour;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+
+    private ZXingScannerView mScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void buttonScanClickFunction(View v){
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        //Do anything with result here :D
+        Log.w("handleResult",result.getText( ));
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        //Resume scanning uncomment below
+        //mScannerView.resumeCameraPreview(this);
+    }
+
+}
