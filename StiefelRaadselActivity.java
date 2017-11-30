65cf4e42692fcc8452d27feba6f9440d07c16a85
Twan <tb_munster@hotmail.com>
2017-11-30 23:27:19 +0100

    diploma toegevoegd nog wel een issue met opslaan van de letters

--- StiefelRaadselActivity.java
+++ StiefelRaadselActivity.java
@@ -1,22 +1,38 @@
 package com.casasolutions.mapsapptour;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StiefelRaadselActivity extends AppCompatActivity {
 
     private String answer;
+    private ImageView diplomaView;
+    private EditText mEditView;
+    private TextView mTextView;
+    private String editTextValue;
+    private String TEXT_VIEW_KEY;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stiefel_raadsel);
 
+
+        if(savedInstanceState != null){
+            mTextView = (TextView) findViewById(R.id.editText1);
+        }
     }
 
     public void buttonCheckAnswerFunction(View v)
@@ -30,6 +46,45 @@ public class StiefelRaadselActivity extends AppCompatActivity {
             answer = answer + mEdit.getText().toString();
         }
         Toast.makeText(this, " Antwoord ingevuld is: " + answer, Toast.LENGTH_LONG).show();
+
+        //If answer is correct show Diploma
+        if (answer.toUpperCase().equals("STIEFELTOCHTDIPLOMA!")){
+            Toast.makeText(this, " Antwoord correct!!, je hebt een diploma verdiend!", Toast.LENGTH_LONG).show();
+            diplomaView = (ImageView) findViewById(R.id.diplomaView);
+            diplomaView.setVisibility(View.VISIBLE);
+        }else{
+            Toast.makeText(this, " Antwoord: " + answer + " is fout, probeer het nog een keer", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void goHomeFunction(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mEditView != null) {
+            editTextValue = mEditView.getText().toString();
+            mTextView.setText(editTextValue);
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        mTextView.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mTextView != null) {
+            outState.putString(TEXT_VIEW_KEY, mTextView.getText().toString());
+        }
+        // call superclass to save any view hierarchy
+        super.onSaveInstanceState(outState);
     }
 }
 
